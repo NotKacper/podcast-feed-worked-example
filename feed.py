94cbf3c2c08ebccc8cc6a2e6e02b19f7ed3f76dd
Kacper Piotr Lubczynski <116844039+NotKacper@users.noreply.github.com>
2024-02-07 17:02:01 +0000

    updates

--- feed.py
+++ feed.py
@@ -11,7 +11,32 @@ rss_element = xml_tree.Element('rss', { # this creates an rss element - an eleme
 
 channel_element = xml_tree.SubElement(rss_element, 'channel') # creates a channel tag inside of the main rss element
 
+link_prefix = yaml_data['link']
+
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title'] # creates a title sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format'] # creates a format sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle'] # creates a subtitle sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author'] # creates a author sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'itunes:image', {'href':link_prefix + yaml_data['image']}) # creates a image sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language'] # creates a language sub element inside the channel element.
+xml_tree.SubElement(channel_element, 'link').text = link_prefix # creates a link sub element inside the channel element.
+
+xml_tree.SubElement(channel_element, 'itunes:category', {'text':yaml_data['category']}) # creates a link sub element inside the channel element.
+
+#reads every entry from the item category in the yaml file.
+for item in yaml_data['item']:
+    item_element = xml_tree.SubElement(channel_element, 'item')
+    xml_tree.SubElement(item_element, 'title').text = item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(item_element, 'description').text = item['description']
+    xml_tree.SubElement(item_element, 'itnues:duration').text = item['duration']
+    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
+
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+        'url': link_prefix + item['file'],
+        'type': 'audio/mpeg',
+        'length': item['length']
+    })
 
 output_tree = xml_tree.ElementTree(rss_element) # creates an XML tree representing the XML file.
 
